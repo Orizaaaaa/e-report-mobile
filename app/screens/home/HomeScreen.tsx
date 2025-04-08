91dc57412ae5ec1ec8e3b80bceba8879a4703b1f
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-04-09 01:25:11 +0700

    finish all

---
+++ app/screens/home/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Hallo</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
---
+++ app/screens/reports/ReportScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+
+const ReportScreen = () => {
+    return (
+        <View>
+            <Text>Hallo</Text>
+        </View>
+    )
+}
+
+export default ReportScreen
