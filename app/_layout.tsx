15badb0c581ec6912e751f6ed6b6da11ddcb6012
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-04-12 22:48:40 +0700

    finish all

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,41 @@
-import { Stack } from "expo-router";
+// app/_layout.tsx
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+const iconMap = {
+  index: 'home',
+  profile: 'person',
+} as const;
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          const iconName = iconMap[route.name as keyof typeof iconMap];
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
 
-export default function RootLayout() {
-  return <Stack />;
+        tabBarStyle: {
+          backgroundColor: '#ffffff',
+          borderRadius: 20,
+          height: 70,
+          paddingBottom: 5,
+          paddingTop: 8,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: -2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          elevation: 10,
+          position: 'absolute',
+          bottom: 15,
+          marginHorizontal: 20 // Android
+        },
+
+        headerShown: false,
+
+      })}
+    >
+      <Tabs.Screen name="index" options={{ title: 'Beranda' }} />
+      <Tabs.Screen name="report" options={{ title: 'Laporan' }} />
+    </Tabs>
+  );
 }
---
+++ app/report.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+
+const reportScreen = () => {
+    return (
+        <View>
+            <Text>Hallo ini adalah halaman report anjing</Text>
+        </View>
+    )
+}
+
+export default reportScreen
